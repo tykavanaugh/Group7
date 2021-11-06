da9c102e672a8df507c30f9ffe78d465a506697b
uabadami <89279056+uabadami@users.noreply.github.com>
2021-11-05 21:09:19 -0400

    Update torTest.py

--- torTest.py
+++ torTest.py
@@ -12,11 +12,7 @@ with TorRequests() as tor_requests:
 	with tor_requests.get_session() as sess:
 		print(sess.get("http://httpbin.org/ip").json()) # new session, new IP
 		print(sess.get("http://httpbin.org/ip").json())
-	print("renew circuit")
-	with tor_requests.get_session() as sess:
-		print(sess.get("http://httpbin.org/ip").json())
-		print(sess.get("http://httpbin.org/ip").json())
-
+	
 # Harder/more customizable way to do requests below.
 
 # from torpy import TorClient
